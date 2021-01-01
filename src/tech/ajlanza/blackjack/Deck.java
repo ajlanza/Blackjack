a921b669671cee882a21a0b6b0544a1dbe2b0c23
ajlanza <61472953+ajlanza@users.noreply.github.com>
2020-12-31 19:16:57 -0500

    refactored with List and ArrayList

--- src/tech/ajlanza/blackjack/Deck.java
+++ src/tech/ajlanza/blackjack/Deck.java
@@ -1,2 +1,47 @@
-package tech.ajlanza.blackjack;public class Deck {
+package tech.ajlanza.blackjack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+
+//    public static final int DECK_SIZE = 52;
+//    private Card[] cards;
+    private List<Card> cards;
+
+    public Deck() {
+//        this.setCards( new Card[DECK_SIZE] );
+        this.setCards( new ArrayList<>());
+        int i = 0;
+        for(Suit suit: Suit.values()){
+            for(Face face: Face.values()){
+//                this.getCards()[i] = new Card(face, suit);
+                this.getCards().add(new Card(face, suit));
+                i++;
+            }
+        }
+    }
+
+    public List<Card> getCards(){
+        return cards;
+    }
+
+    public void setCards(List<Card> cards){
+        this.cards = cards;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (Card c : this.getCards()){
+            sb.append(String.format("%s,", c));
+        }
+//        if (this.getCards().length > 0) {
+        if (this.getCards().size() > 0){
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
--- src/tech/ajlanza/blackjack/Main.java
+++ src/tech/ajlanza/blackjack/Main.java
@@ -2,20 +2,7 @@ package tech.ajlanza.blackjack;
 
 public class Main {
     public static void main(String[] args) {
-        // No argument constructor
-        Card defaultCard = new Card();
-        System.out.println(defaultCard);
-        // Two argument constructor
-        Card sevenSpades = new Card(Face.SEVEN, Suit.SPADES);
-        Card jackClubs = new Card(Face.JACK, Suit.CLUBS);
-        System.out.println(sevenSpades);
-        System.out.println(jackClubs);
-        // Four argument constructor
-        Card aceOfSpades =  new Card(Face.ACE, Suit.SPADES, 1, true);
-        Card threeOfDiamonds =  new Card(Face.THREE, Suit.DIAMONDS, 3, false);
-        System.out.println(aceOfSpades);
-        System.out.println(sevenSpades.getCardFace());
-        sevenSpades.setSuit(Suit.DIAMONDS);
-        System.out.println(sevenSpades);
+        Deck deck = new Deck();
+        System.out.println(deck);
     }
 }
