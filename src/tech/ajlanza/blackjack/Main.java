5186c9d8240e671b5f10e99d9b1f68a87c147af2
ajlanza <61472953+ajlanza@users.noreply.github.com>
2021-01-05 20:52:28 -0500

    check user input to see if they want to play

--- src/tech/ajlanza/blackjack/Main.java
+++ src/tech/ajlanza/blackjack/Main.java
@@ -1,27 +1,38 @@
 package tech.ajlanza.blackjack;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game();
-        System.out.println("The initial Game");
-        System.out.println(game);
+        Main main = new Main();
+        main.play();
+    }
 
-        System.out.println("The player's turn");
-        while(game.isPlayerTurn()) {
-            if(game.getPlayer().getValue() < 18) {
-                System.out.println("Hit");
-                game.playerMove(Game.Action.HIT);
-            } else {
-                System.out.println("STAND");
-                game.playerMove(Game.Action.STAND);
+    public void play() {
+        boolean keepPlaying = true;
+        Scanner scanner = new Scanner(System.in);
+        while(keepPlaying) {
+            String menuChoice = getMenuChoice(scanner);
+            keepPlaying = menuChoice.equalsIgnoreCase("s");
+            if(keepPlaying) {
+
+                System.out.println("This is one game play here");
             }
-            System.out.println(game.getPlayer());
         }
+        System.out.println("Thanks for playing!");
+    }
 
-        System.out.println("The dealer's Turn");
-        game.dealerMove();
-
-        System.out.println(game.outcome());
-        System.out.println(game);
+    public String getMenuChoice(Scanner scanner) {
+        String menuChoice = " ";
+        boolean isValid = false;
+        while(!isValid) {
+            System.out.println("Welcome to Blackjack!\n\n Press s to start or e to exit");
+            menuChoice = scanner.next();
+            isValid = menuChoice.equalsIgnoreCase("s") || menuChoice.equalsIgnoreCase("e");
+            if(!isValid) {
+                System.out.println("Invalid choice!");
+            }
+        }
+        return menuChoice;
     }
 }
